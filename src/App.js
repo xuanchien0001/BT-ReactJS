a72d2a51065921496c4cdda4aa26511d55415302
xuanchienueh <xuanchien.ueh@gmail.com>
2022-06-06 09:09:54 +0700

    1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="">lsdfládf</h1>
+        <button className="btn btn-danger">click</button>
+      </div>
+    );
+  }
+}
---
+++ src/redux/action/ActionXucXac/action.js
@@ -0,0 +1,1 @@
+const sdf = "lk";
